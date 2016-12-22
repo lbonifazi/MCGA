14fdde605b77195b0895256ea532f6f8fd4f23cc
Ezequiel Pochiero <epochiero@gmail.com>
2016-12-22 12:52:56 -0300

    price

--- SalesBooks/DAL/Entities/Price.cs
+++ SalesBooks/DAL/Entities/Price.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace DAL.Entities
     public class Price
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int BookId { get; set; }
         public DateTime Date { get; set; }
         public decimal Cost { get; set; }
